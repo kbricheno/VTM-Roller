93e7e783203cee506c5f8f366b6ab86557d589cc
kbricheno <katherine.bricheno@gmail.com>
2024-06-06 17:56:03 +0100

    scrollable results
    created scrollable canvas class that can be populated with buttons (for future re-rolling) or labels to display roll results

--- diceHandling.py
+++ diceHandling.py
@@ -2,19 +2,7 @@ from random import randint
 from math import floor
 
 
-def get_dice(normal):
-
-    while True:
-        dice = input("\nNormal dice: ") if normal else input("Hunger dice: ")
-        if not dice.isdigit():
-            print("Please enter a whole number.\n")
-            continue
-        else:
-            return dice
-
-
 def roll_dice(dice):
-
     diceList = []
     for i in range(int(dice)):
         result = randint(1, 10)
@@ -23,12 +11,12 @@ def roll_dice(dice):
     return diceList
 
 
-def evaluate_dice(_normalDiceList, _hungerDiceList):
+def evaluate_dice(normalDiceList, hungerDiceList):
     resultTypes = {"successes": 0, "hungerSuccesses": 0,
                    "crits": 0, "messyCrits": 0,
                    "failures": 0, "hungerFailures": 0, "bestialFailures": 0}
 
-    for result in _normalDiceList:
+    for result in normalDiceList:
         if result == 10:
             resultTypes["crits"] += 1
         elif result >= 6:
@@ -36,7 +24,7 @@ def evaluate_dice(_normalDiceList, _hungerDiceList):
         else:
             resultTypes["failures"] += 1
 
-    for result in _hungerDiceList:
+    for result in hungerDiceList:
         if result == 10:
             resultTypes["messyCrits"] += 1
         elif result >= 6:
@@ -49,7 +37,7 @@ def evaluate_dice(_normalDiceList, _hungerDiceList):
     return resultTypes
 
 
-def print_results(results, _normalDiceList, _hungerDiceList):
+def print_results(results):
     # for ease of readability below
     successes = results["successes"]
     hungerSuccesses = results["hungerSuccesses"]
@@ -62,48 +50,42 @@ def print_results(results, _normalDiceList, _hungerDiceList):
     combinedSuccesses = successes + hungerSuccesses
     combinedFailures = failures + hungerFailures + bestialFailures
 
-    print()
-    if _normalDiceList:
-        print(f"Normal dice ({len(_normalDiceList)}): {_normalDiceList}")
-    if _hungerDiceList:
-        print(f"Hunger dice ({len(_hungerDiceList)}): {_hungerDiceList}")
-    print()
+    writtenResults = ""
 
     if combinedSuccesses + crits + messyCrits == 0:
-        print(f"Oh no! 0 successes.\n"
-              f"Failures: {combinedFailures} ({failures} normal, {hungerFailures} hunger, and "
-              f"{bestialFailures} bestial)")
+        writtenResults += (f"Oh no! 0 successes.\n"
+                           f"Failures: {combinedFailures} ({failures} normal, {hungerFailures} hunger, and "
+                           f"{bestialFailures} bestial)")
 
     elif crits + messyCrits >= 2:
         if messyCrits >= 1:
             additionalSuccesses = (floor((crits + messyCrits) / 2) * 4) + ((crits + messyCrits) % 2)
-            print(f"Messy crit!\n"
-                  f"Successes: {combinedSuccesses + additionalSuccesses} ({combinedSuccesses} success(es) + "
-                  f"{messyCrits} hunger and {crits} normal crit(s)\n"
-                  f"Failures: {combinedFailures} ({failures} normal, {hungerFailures} hunger, and "
-                  f"{bestialFailures} bestial)")
+            writtenResults += (f"Messy crit!\n"
+                               f"Successes: {combinedSuccesses + additionalSuccesses} ({combinedSuccesses} "
+                               f"success(es) + {messyCrits} hunger and {crits} normal crit(s))\n"
+                               f"Failures: {combinedFailures} ({failures} normal, {hungerFailures} hunger, and "
+                               f"{bestialFailures} bestial)")
         else:
             additionalSuccesses = (floor(crits / 2) * 4) + (crits % 2)
-            print(f"Crit!\n" +
-                  f"Successes: {combinedSuccesses + additionalSuccesses} ({combinedSuccesses} success(es) + {crits} "
-                  f"normal crits)\n" +
-                  f"Failures: {combinedFailures} ({failures} normal, {hungerFailures} hunger, and "
-                  f"{bestialFailures} bestial)")
+            writtenResults += (f"Crit!\n" +
+                               f"Successes: {combinedSuccesses + additionalSuccesses} ({combinedSuccesses} "
+                               f"success(es) + {crits} normal crit(s))\n" +
+                               f"Failures: {combinedFailures} ({failures} normal, {hungerFailures} hunger, and "
+                               f"{bestialFailures} bestial)")
 
     else:
-        print(f"No crits.\n"
-              f"Successes: {combinedSuccesses + crits + messyCrits}\n"
-              f"Failures: {combinedFailures} ({failures} normal, {hungerFailures} hunger, and "
-              f"{bestialFailures} bestial)")
+        writtenResults += (f"No crits.\n"
+                           f"Successes: {combinedSuccesses + crits + messyCrits}\n"
+                           f"Failures: {combinedFailures} ({failures} normal, {hungerFailures} hunger, and "
+                           f"{bestialFailures} bestial)")
 
-    print()
+    return writtenResults
 
 
-def get_reroll_values(_normalDiceList, priorResults):
-
+def get_reroll_values(normalDiceList, priorResults):
     translatedList = []
 
-    for result in _normalDiceList:
+    for result in normalDiceList:
         if result == 10:
             translatedList.append("crit")
         elif result >= 6:
@@ -112,7 +94,7 @@ def get_reroll_values(_normalDiceList, priorResults):
             translatedList.append("fail")
 
     print(f"\nHere are your normal dice results: "
-          f"\nRaw values: {_normalDiceList}"
+          f"\nRaw values: {normalDiceList}"
           f"\nResults: {translatedList}")
 
     while True:
@@ -155,7 +137,7 @@ def validate_reroll_values(chosenValues, priorResults):
     return True, ""
 
 
-def pop_rerolls(_normalDiceList, rerollValues):
+def pop_rerolls(normalDiceList, rerollValues):
     failsPopped = []
     successesPopped = []
     critsPopped = []
@@ -165,31 +147,31 @@ def pop_rerolls(_normalDiceList, rerollValues):
         popCounter = 0  # counter for how many fails/successes/crits we want popped
 
         if resultType == 0:
-            for dice in reversed(_normalDiceList):  # reverse list to iterate and modify in the same loop
+            for dice in reversed(normalDiceList):  # reverse list to iterate and modify in the same loop
                 if dice < 6 and popCounter < rerollValues[resultType]:
-                    failsPopped.append(_normalDiceList.pop(_normalDiceList.index(dice)))
+                    failsPopped.append(normalDiceList.pop(normalDiceList.index(dice)))
                     popCounter += 1
         elif resultType == 1:
-            for dice in reversed(_normalDiceList):
+            for dice in reversed(normalDiceList):
                 if 6 <= dice < 10 and popCounter < rerollValues[resultType]:
-                    successesPopped.append((_normalDiceList.pop(_normalDiceList.index(dice))))
+                    successesPopped.append((normalDiceList.pop(normalDiceList.index(dice))))
                     popCounter += 1
         else:
-            for dice in reversed(_normalDiceList):
+            for dice in reversed(normalDiceList):
                 if dice == 10 and popCounter < rerollValues[resultType]:
-                    critsPopped.append((_normalDiceList.pop(_normalDiceList.index(dice))))
+                    critsPopped.append((normalDiceList.pop(normalDiceList.index(dice))))
                     popCounter += 1
 
-    return _normalDiceList
+    return normalDiceList
 
 
-def print_rerolls(rerollList, _normalDiceList):
+def print_rerolls(rerollList, normalDiceList):
     print(f"\nThe results of your re-rolls are: {rerollList}")
-    print(f"New normal dice ({len(_normalDiceList) + len(rerollList)}): "
-          f"{_normalDiceList + rerollList}")
+    print(f"New normal dice ({len(normalDiceList) + len(rerollList)}): "
+          f"{normalDiceList + rerollList}")
     input("\nPress Enter to re-evaluate all of your rolls. ")
 
 
-def clear_results(_normalDiceList, _hungerDiceList):
-    _normalDiceList.clear()
-    _hungerDiceList.clear()
+def clear_results(normalDiceList, hungerDiceList):
+    normalDiceList.clear()
+    hungerDiceList.clear()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import sys
 import tkinter.messagebox
 from tkinter import *
 from tkinter import ttk
-import methods as m
+import diceHandling as d
 
 normalDiceList = []
 hungerDiceList = []
-
 
 # def new_roll():  # roll a fresh set of dice
 #     global normalDiceList, hungerDiceList
@@ -83,10 +83,125 @@ hungerDiceList = []
 # set up main application window & title it
 root = Tk()
 root.title("Vampire: The Masquerade | Dice Roller")
+root.geometry("350x350")
+
+normalDiceImages = {"success": PhotoImage(file="DiceImages/normalSuccess.png"),
+                    "crit": PhotoImage(file="DiceImages/normalCrit.png"),
+                    "fail": PhotoImage(file="DiceImages/normalFail.png")}
+
+hungerDiceImages = {"success": PhotoImage(file="DiceImages/hungerSuccess.png"),
+                    "crit": PhotoImage(file="DiceImages/hungerCrit.png"),
+                    "fail": PhotoImage(file="DiceImages/hungerFail.png"),
+                    "bestialFail": PhotoImage(file="DiceImages/hungerBestialFail.png")}
 
 normalDice = StringVar(value="0")
 hungerDice = StringVar(value="0")
 results = StringVar()
+
+canvases = []
+
+
+class ScrollableCanvas:  # class for readability + i need 2, 1 for normal & 1 for hunger
+    def __init__(self, spawnParent, spawnColumn, spawnRow, normal):
+        # create & configure elements
+        self.normal = normal
+        self.container = Frame(spawnParent)
+        # canvases can have an actual size ("scrollable region") and a screen/view size ("window")
+        self.canvas = Canvas(self.container)
+        self.yScroll = Scrollbar(self.container, orient=VERTICAL, command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.yScroll.set)
+        # create a frame that will contain all the buttons & therefore act as the canvas' scrollable region
+        self.scrollFrame = Frame(self.canvas)
+        # when <Configure>d (when the scrollable frame changes size), update the frame's parent's scrollable region
+        # i will never understand why this must be bound to configure & cannot just be done once after populating frame
+        self.scrollFrame.bind("<Configure>", lambda event: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+
+        # place elements
+        self.container.grid(column=spawnColumn, row=spawnRow)
+        self.canvas.grid(column=0, row=0)
+        self.yScroll.grid(column=1, row=0, sticky=NS)
+        self.canvas.create_window((0, 0), window=self.scrollFrame, anchor=NW)
+
+    def populate_frame(self, dice):
+        global normalDiceImages, hungerDiceImages
+
+        spawnAtColumn = -1
+        spawnAtRow = -1
+
+        for die in dice:
+            spawnAtColumn = (spawnAtColumn + 1) % 3  # create a 3 by X grid to present the dice results in
+            if spawnAtColumn == 0:
+                spawnAtRow += 1
+
+            if self.normal:  # normal-specific function because normal dice must spawn as buttons & with diff images
+                if die == 10:  # assign images to dice depending on their values
+                    spawnWithImage = normalDiceImages["crit"]
+                elif die >= 6:
+                    spawnWithImage = normalDiceImages["success"]
+                else:
+                    spawnWithImage = normalDiceImages["fail"]
+                ttk.Button(self.scrollFrame, text=die, image=spawnWithImage).grid(column=spawnAtColumn, row=spawnAtRow)
+
+            else:  # hunger dice spawn as labels bc user doesn't need to click on them for re-rolling
+                if die == 10:
+                    spawnWithImage = hungerDiceImages["crit"]
+                elif die >= 6:
+                    spawnWithImage = hungerDiceImages["success"]
+                elif die > 1:
+                    spawnWithImage = hungerDiceImages["fail"]
+                else:
+                    spawnWithImage = hungerDiceImages["bestialFail"]
+                ttk.Label(self.scrollFrame, image=spawnWithImage).grid(column=spawnAtColumn, row=spawnAtRow)
+
+        self.finalise_canvas()
+
+    def finalise_canvas(self):  # set width & potentially disable scrollbar
+        # set the width & height to show 3x3 dice (i think most people will roll 9 or fewer dice)
+        dieWidth = self.scrollFrame.winfo_children()[0].winfo_reqwidth()
+        self.canvas.configure(width=dieWidth*3, height=dieWidth*3)
+        # don't need to scroll if there are 9 or fewer dice
+        if len(self.scrollFrame.winfo_children()) <= 9:
+            self.yScroll.grid_forget()
+
+    def destroy_canvas(self):
+        self.container.destroy()
+        self.canvas.destroy()
+        self.yScroll.destroy()
+        self.scrollFrame.destroy()
+
+
+def rolling(*args):  # *args here because this method can be called by both a button (no argument) and
+    # a <Return> event (event argument), so it must be able to receive either 0 or 1 argument
+    global normalDiceList, hungerDiceList
+
+    # discard any previous results
+    for canvas_ in canvases:
+        canvas_.destroy_canvas()
+    canvases.clear()
+
+    # receive and validate input (ensure blanks = 0, ensure total > 0)
+    submit_validation()
+
+    # roll the dice
+    normalDiceList = d.roll_dice(int(normalDice.get()))
+    hungerDiceList = d.roll_dice(int(hungerDice.get()))
+
+    # generate scrollable canvas of buttons for normal dice, scrollable canvas of labels for hunger dice
+    if normalDiceList:
+        normalScrollCanvas = ScrollableCanvas(mainFrame, 0, 4, True)
+        normalScrollCanvas.populate_frame(normalDiceList)
+        canvases.append(normalScrollCanvas)
+
+    if hungerDiceList:
+        hungerScrollCanvas = ScrollableCanvas(mainFrame, 1, 4, False)
+        hungerScrollCanvas.populate_frame(hungerDiceList)
+        canvases.append(hungerScrollCanvas)
+
+    # evaluate the dice
+    evaluatedResults = d.evaluate_dice(normalDiceList, hungerDiceList)
+
+    # populate label with evaluations
+    results.set(d.print_results(evaluatedResults))
 
 
 def instant_validation(attemptedInput, action):  # validation that happens while the user inserts characters
@@ -112,50 +227,38 @@ def submit_validation():  # validation that happens only once the user has submi
         tkinter.messagebox.showerror("Invalid Input", "Please enter at least one number.")
 
 
-def rolling(*args):
-    submit_validation()
-    # roll the dice
-    # evaluate the dice
-    # generate list of buttons for normal dice, list of labels for hunger dice
-    # generate label with evaluations
-
-
 def normal_dice_input(event):  # auto highlight existing text inside the entry field for easy overwriting
     normalEntry.selection_range(0, END)
 
 
-def hunger_dice_input(event):  # auto highlight existing text inside the entry field for easy overwriting
+def hunger_dice_input(event):  # same as above but for other entry field
     hungerEntry.selection_range(0, END)
 
 
-# frame widget that will hold everything else
-mainFrame = ttk.Frame(root, padding=25, relief="groove")
-# "grid" places the created widget in the window
-mainFrame.grid()
-# these 2 tell the widget to fill the window if it's resized
+# frame widget that will hold everything else. stored as a variable so it can be referenced as a parent
+mainFrame = ttk.Frame(root, padding=130, relief="groove")
+mainFrame.grid()  # "grid" places the created widget in the window
+
+# these 2 tell the widget to re-centre in the window if it's resized
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-normalLabel = ttk.Label(mainFrame, text="NORMAL:")
-hungerLabel = ttk.Label(mainFrame, text="HUNGER:")
-normalLabel.grid(column=0, row=0)
-hungerLabel.grid(column=0, row=1)
 
-normalEntry = ttk.Entry(mainFrame, textvariable=normalDice, validate="key")
-normalEntry["validatecommand"] = normalEntry.register(instant_validation), "%P", "%d"
-normalEntry.grid(column=1, row=0)
-hungerEntry = ttk.Entry(mainFrame, textvariable=hungerDice, validate="key")
-hungerEntry["validatecommand"] = hungerEntry.register(instant_validation), "%P", "%d"
-hungerEntry.grid(column=1, row=1)
-
-
-normalEntry.bind("<FocusIn>", normal_dice_input)
-hungerEntry.bind("<FocusIn>", hunger_dice_input)
-
-rollButton = ttk.Button(mainFrame, text="Roll", command=rolling)
-rollButton.grid(column=1, row=2)
+# create additional labels and buttons
+ttk.Label(mainFrame, text="NORMAL:").grid(column=0, row=0)  # input label
+ttk.Label(mainFrame, text="HUNGER:").grid(column=0, row=1)  # input label
+ttk.Label(mainFrame, textvariable=results).grid(column=0, columnspan=2, row=3)  # temporary output label
+ttk.Button(mainFrame, text="Roll", command=rolling).grid(column=0, columnspan=2, row=2)  # submit button
 root.bind("<Return>", rolling)  # also bind return (no matter what is selected) to the roll method
 
-resultsLabel = ttk.Label(mainFrame, textvariable=results)
-resultsLabel.grid(column=1, row=3)
+normalEntry = ttk.Entry(mainFrame, width=4, textvariable=normalDice, validate="key")  # validate on key press
+# register essentially holds the proposed edit and puts it through validation before setting the stringvar
+normalEntry["validatecommand"] = normalEntry.register(instant_validation), "%P", "%d"  # %P = key pressed, %d = action
+normalEntry.grid(column=1, row=0)
+normalEntry.bind("<FocusIn>", normal_dice_input)
+
+hungerEntry = ttk.Entry(mainFrame, width=4, textvariable=hungerDice, validate="key")
+hungerEntry["validatecommand"] = hungerEntry.register(instant_validation), "%P", "%d"
+hungerEntry.grid(column=1, row=1)
+hungerEntry.bind("<FocusIn>", hunger_dice_input)
 
 root.mainloop()
